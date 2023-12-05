76798e48e4bee10867ad1b4cea4b9e2805555d07
selahaddin03 <149465780+selahaddin03@users.noreply.github.com>
2023-12-05 19:38:31 +0300

    Proje dosyası ekle.

---
+++ DegerliParemetreler/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DegerliParemetreler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sinifim s= new sinifim();
+            s.selamla();
+            s.selamla("hasan");
+        }
+    }
+    class sinifim
+    {
+        public void selamla(string ad = "OBEZ HASAN")
+        {
+            Console.WriteLine("Merhaba :"+ad);
+        }
+
+        
+        
+    }
+}
